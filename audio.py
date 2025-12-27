294c312d47f222d05e71ccc64bac69d5ef6be856
jvx <jonvx0@gmail.com>
2025-12-27 18:11:03 -0500

    feat: audio recording

--- audio.py
+++ audio.py
@@ -1,8 +1,30 @@
 import sounddevice as sd
 import numpy
+import soundfile as sf
+import queue
+import sys
+assert numpy
 
 device : int = 0
 samplerate : int = 48000
-duration : int = 10 #seconds
+filename : str = "sound!.wav"
 
-sd.InputStream.device = 
+q = queue.Queue()
+
+def callback(indata, frames, time, status):
+    if status:
+        print(status, file=sys.stderr)
+    q.put(indata.copy())
+
+try:
+    with sf.SoundFile(filename, mode='x', samplerate=samplerate, channels=1) as file:
+        with sd.InputStream(samplerate=samplerate, device=device,channels=1, callback=callback):
+            print("#" * 80)
+            print("ctrl+c to stop recording")
+            print("#" * 80)
+            while True:
+                file.write(q.get())
+except KeyboardInterrupt:
+    print('finished recording: ' + filename)
+
+
